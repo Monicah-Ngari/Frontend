dba18f54f67a4d897bb25bdcc10be3dacc2757b9
Monicah-Ngari <wakarimangari@gmail.com>
2024-01-06 17:55:00 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,25 +3,22 @@ function displayForecast() {
   let forecastHtml = "";
 
   days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-            <div class="row">
-                <div class="col-2">
-                    <div class="forecast-date"> ${day}</div>
-                    <div class="weather-icon" id="weather-icon"> <img src="src/weather.png" alt="" width="35" /></div>
-                    <div class="forecast-temp">
-                        <span class="current-forecast"><strong>18°</strong></span>
-                        <span class="future-forecast"> 12°</span>
-                    </div>
-
-                </div>
-
-            </div>
-            `;
+    forecastHtml += `
+      <div class="row">
+        <div class="col-2">
+          <div class="forecast-date">${day}</div>
+          <div class="weather-icon" id="weather-icon"><img src="src/weather.png" alt="" width="35" /></div>
+          <div class="forecast-temp">
+            <span class="current-forecast"><strong>18°</strong></span>
+            <span class="future-forecast">12°</span>
+          </div>
+        </div>
+      </div>
+      `;
   });
-  let forecast = document.querySelector("#weather-forecast");
 
+  let forecast = document.querySelector("#weather-forecast");
   forecast.innerHTML = forecastHtml;
 }
+
 displayForecast();
