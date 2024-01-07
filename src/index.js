4542dc95bc611138717570c21be8d36f8f92b31f
Monicah-Ngari <wakarimangari@gmail.com>
2024-01-07 09:58:06 +0300

    comkmit

--- src/index.js
+++ src/index.js
@@ -9,9 +9,13 @@ let myCity = document.querySelector("#my-input");
 myCity.addEventListener("submit", handleClick);
 
 // Change city
-
-consonsole.log(axios);
-//Change Weather conditons
+function searchCity(city) {
+  let apiKey = "1a2a473db97faf41f0088oe8t98271ff";
+  let myUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric `;
+  axios.get(myUrl).then(refreshWeather);
+  consonsole.log(axios);
+}
+// Change Weather conditons
 
 function displayForecast() {
   let days = ["Teu", "Wed", "Thur", "Fri", "Sat"];
