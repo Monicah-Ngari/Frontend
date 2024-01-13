f406e99b08c60a44d0bd130407c23ee35896451f
Monicah-Ngari <wakarimangari@gmail.com>
2024-01-13 04:54:05 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function refreshWeather(response) {
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
 
-  cityElement = data.response.city;
+  cityElement = response.data.city;
   weatherCondition.innerHTML = response.data.condition.description;
   currentHumidity.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
