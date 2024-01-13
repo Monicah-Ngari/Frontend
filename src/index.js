d06f125c890af3f96a490f6f014f55ef156ec1d0
Monicah-Ngari <wakarimangari@gmail.com>
2024-01-13 05:24:31 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ function formatDay(timestamp) {
 }
 
 // Forecast day
-function displayForecast() {
+function displayForecast(response) {
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
