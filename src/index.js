945d0be141eac9c6e4ec6b0744e8a26e8f9e50cb
Monicah-Ngari <wakarimangari@gmail.com>
2024-01-15 19:35:50 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ function formatDate(date) {
 function searchCity(event) {
   event.preventDefault();
   let city = document.getElementById("city-input").value;
-  let heading = document.querySelector("#city");
+  let heading = document.querySelectElementbyId("city");
   console.log(city);
   heading.innerHTML = city;
   let apiKey = "1a2a473db97faf41f0088oe8t98271ff";
